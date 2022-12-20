0e76a229e29956d1f906a43e5870724676c8714a
Braden Walker <bradenawalker@gmail.com>
2022-12-20 13:53:12 -0800

    corrections to explanation of priorities

--- priority.go
+++ priority.go
@@ -1,14 +1,14 @@
 package backpressure
 
-// Priority is the importance of a request, and types in this package prefer higher priority
-// requests.
+// Priority is the importance of a request. Types in this package prefer higher priority requests.
 //
 // Priorities are non-negative integers and are numbered in decreasing order of priority.
 // `Priority(0)` is the highest priority, meaning it is preferred over all other priorities.
-// `Priority(1)` is higher priority than all priorities except `Priority(1)`, and so on.
+// `Priority(1)` is higher priority than all priorities except `Priority(0)`, and so on.
 //
-// Types in this package generally have overhead per-priority, and so it's recommended to use a
-// relatively small number of priorities. Around four works well in practice, for example:
+// Types in this package generally have overhead per-priority and function best when priorities are
+// quite coarse, and so it's recommended to use a relatively small number of priorities. Around four
+// works well in practice, for example:
 //
 // Critical - A user is waiting and critical user flows are functional, if severely degraded, if
 // only Critical requests are served.
