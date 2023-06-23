67b85de72eb56cc3022d43816c5ad056bc58fa9f
Braden Walker <bradenawalker@gmail.com>
2023-06-23 00:24:45 -0400

    comment on codel

--- codel.go
+++ codel.go
@@ -8,6 +8,13 @@ import (
 	"github.com/bradenaw/juniper/container/deque"
 )
 
+// CoDel ("Controlled Delay") is a queue that adapts its behavior to load: healthy queues are for
+// absorbing short-term spikes and are frequently emptied. Unhealthy queues are persistently loaded.
+// CoDel is, under normal circumstances, a regular-old FIFO (first in, first out) queue. When CoDel
+// hasn't been emptied in the last longTimeout, it switches to shortTimeout and LIFO (last in,
+// first out). This assumes that more recent requests are more valuable, which is often the case
+// in real-world systems: old requests may have already timed out and retried. Once the CoDel
+// empties again, it switches back to FIFO.
 type codel[T any] struct {
 	shortTimeout time.Duration
 	longTimeout  time.Duration
