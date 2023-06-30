3d00acbdfc1b5d844f0c6c5900de9a220f31383b
Braden Walker <bradenawalker@gmail.com>
2023-06-29 21:13:32 -0400

    move options to AdaptiveThrottleOptions

--- adaptive_throttle.go
+++ adaptive_throttle.go
@@ -8,10 +8,11 @@ import (
 	"time"
 )
 
-// MarkAccepted marks the given err as "accepted" by the backend for the purposes of
+// AcceptedError wraps the given err as "accepted" by the backend for the purposes of
 // AdaptiveThrottle. This should be done for regular protocol errors that do not mean the backend is
-// unhealthy.
-func MarkAccepted(err error) error { return errAccepted{inner: err} }
+// unhealthy, for example a precondition failure as part of an optimistic update or a rejection for
+// a particular request being too large.
+func AcceptedError(err error) error { return errAccepted{inner: err} }
 
 type errAccepted struct{ inner error }
 
@@ -32,40 +33,83 @@ func (err errAccepted) Unwrap() error { return err.inner }
 //
 // More on adaptive throttles in https://sre.google/sre-book/handling-overload/
 type AdaptiveThrottle struct {
-	k float64
+	k            float64
+	minPerWindow float64
 
 	m        sync.Mutex
 	requests []windowedCounter
 	accepts  []windowedCounter
 }
 
+// Additional options for the AdaptiveThrottle type. These options do not frequently need to be
+// tuned as the defaults work in a majority of cases.
+type AdaptiveThrottleOption struct {
+	f func(*adaptiveThrottleOptions)
+}
+
+type adaptiveThrottleOptions struct {
+	k       float64
+	minRate float64
+	d       time.Duration
+}
+
+// AdaptiveThrottleRatio sets the ratio of the measured success rate and the rate that the throttle
+// will admit. For example, when k is 2 the throttle will allow twice as many requests to actually
+// reach the backend as it believes will succeed. Higher values of k mean that the throttle will
+// react more slowly when a backend becomes unhealthy, but react more quickly when it becomes
+// healthy again, and will allow more load to an unhealthy backend. k=2 is usually a good place to
+// start, but backends that serve "cheap" requests (e.g. in-memory caches) may need a lower value.
+func AdaptiveThrottleRatio(k float64) AdaptiveThrottleOption {
+	return AdaptiveThrottleOption{func(opts *adaptiveThrottleOptions) {
+		opts.k = k
+	}}
+}
+
+// AdaptiveThrottleMinimumRate sets the minimum number of requests per second that the adaptive
+// throttle will allow (approximately) through to the upstream, even if every request is failing.
+// This is important because this is how the adaptive throttle 'learns' when the upstream becomes
+// healthy again.
+func AdaptiveThrottleMinimumRate(x float64) AdaptiveThrottleOption {
+	return AdaptiveThrottleOption{func(opts *adaptiveThrottleOptions) {
+		opts.minRate = x
+	}}
+}
+
+// AdaptiveThrottleWindow sets the time window over which the throttle remembers requests for use in
+// figuring out the success rate.
+func AdaptiveThrottleWindow(d time.Duration) AdaptiveThrottleOption {
+	return AdaptiveThrottleOption{func(opts *adaptiveThrottleOptions) {
+		opts.d = d
+	}}
+}
+
 // NewAdaptiveThrottle returns an AdaptiveThrottle.
 //
 // priorities is the number of priorities that the throttle will accept. Giving a priority outside
 // of `[0, priorities)` will panic.
-//
-// k is the ratio of the measured success rate and the rate that the throttle will admit. For
-// example, when k is 2 the throttle will allow twice as many requests to actually reach the backend
-// as it believes will succeed. Higher values of k mean that the throttle will react more slowly
-// when a backend becomes unhealthy, but react more quickly when it becomes healthy again, and will
-// allow more load to an unhealthy backend. k=2 is usually a good place to start, but backends that
-// serve "cheap" requests (e.g. in-memory caches) may need a lower value.
-//
-// d is the time window over which the throttle remembers requests for use in figuring out the
-// success rate.
-func NewAdaptiveThrottle(priorities int, k float64, d time.Duration) *AdaptiveThrottle {
+func NewAdaptiveThrottle(priorities int, options ...AdaptiveThrottleOption) *AdaptiveThrottle {
+	opts := adaptiveThrottleOptions{
+		d:       time.Minute,
+		k:       2,
+		minRate: 1,
+	}
+	for _, option := range options {
+		option.f(&opts)
+	}
+
 	now := time.Now()
 	requests := make([]windowedCounter, priorities)
 	accepts := make([]windowedCounter, priorities)
 	for i := range requests {
-		requests[i] = newWindowedCounter(now, d/10, 10)
-		accepts[i] = newWindowedCounter(now, d/10, 10)
+		requests[i] = newWindowedCounter(now, opts.d/10, 10)
+		accepts[i] = newWindowedCounter(now, opts.d/10, 10)
 	}
 
 	return &AdaptiveThrottle{
-		k:        k,
-		requests: requests,
-		accepts:  accepts,
+		k:            opts.k,
+		requests:     requests,
+		accepts:      accepts,
+		minPerWindow: opts.minRate * opts.d.Seconds(),
 	}
 }
 
@@ -88,7 +132,7 @@ func WithAdaptiveThrottle[T any](
 	}
 	at.m.Unlock()
 
-	rejectionProbability := math.Max(0, (requests-at.k*accepts)/(requests+1))
+	rejectionProbability := math.Max(0, (requests-at.k*accepts)/(requests+at.minPerWindow))
 
 	if rand.Float64() < rejectionProbability {
 		var zero T
